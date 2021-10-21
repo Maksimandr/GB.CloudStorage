82fc0cd604c93447cf5de5c34a8bce994fae4647
Maksimandr <maksimandr@mail.ru>
2021-10-21 18:33:00 +0500

    пробую реализовать режим автоматического мониторинга изменений в директории

--- src/main/java/client/CloudStorageClient.java
+++ src/main/java/client/CloudStorageClient.java
@@ -10,9 +10,11 @@ import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.codec.LengthFieldPrepender;
 import io.netty.handler.codec.bytes.ByteArrayDecoder;
 import io.netty.handler.codec.bytes.ByteArrayEncoder;
+import org.apache.commons.io.monitor.FileAlterationListener;
+import org.apache.commons.io.monitor.FileAlterationMonitor;
+import org.apache.commons.io.monitor.FileAlterationObserver;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -22,11 +24,11 @@ public class CloudStorageClient {
 
     private static final File clientDirectory = new File("clientDirectory");
 
-    public static void main(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws Exception {
         new CloudStorageClient().start();
     }
 
-    public void start() throws InterruptedException, IOException {
+    public void start() throws Exception {
         NioEventLoopGroup group = new NioEventLoopGroup();
         Scanner scanner = new Scanner(System.in);
 
@@ -54,6 +56,54 @@ public class CloudStorageClient {
 
             ChannelFuture channelFuture = client.connect("localhost", 9000).sync();
             System.out.println("Client started");
+
+            FileAlterationObserver observer = new FileAlterationObserver("/Users/a17081740/watch");
+            FileAlterationMonitor monitor = new FileAlterationMonitor(1000);
+            FileAlterationListener listener = new FileAlterationListener() {
+                @Override
+                public void onStart(FileAlterationObserver observer) {
+
+                }
+
+                @Override
+                public void onDirectoryCreate(File directory) {
+
+                }
+
+                @Override
+                public void onDirectoryChange(File directory) {
+
+                }
+
+                @Override
+                public void onDirectoryDelete(File directory) {
+
+                }
+
+                @Override
+                public void onFileCreate(File file) {
+                    System.out.println("Created Filename: " + file.getName() + " path:" + file.getAbsolutePath());
+                }
+
+                @Override
+                public void onFileChange(File file) {
+                    System.out.println("Change Filename: " + file.getName() + " path:" + file.getAbsolutePath());
+                }
+
+                @Override
+                public void onFileDelete(File file) {
+                    System.out.println("Delete Filename: " + file.getName() + " path:" + file.getAbsolutePath());
+                }
+
+                @Override
+                public void onStop(FileAlterationObserver observer) {
+
+                }
+            };
+
+            observer.addListener(listener);
+            monitor.addObserver(observer);
+            monitor.start();
 
             System.out.println("Команды для работы:");
             System.out.println("all del - удаляет все файлы на удаленной стороне");
